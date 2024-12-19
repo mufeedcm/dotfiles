267a64a4065aa41125326284ce94b0abf6f89e28
mufeedcm <muhammedmufeedcm@gmail.com>
2024-12-19 07:10:36 +0530

    add

--- linux/nvim/.config/nvim/lua/mufeedcm/plugins/init.lua
+++ linux/nvim/.config/nvim/lua/mufeedcm/plugins/init.lua
@@ -154,28 +154,28 @@ return {
 	},
 	-- ----------------------------------------
 	-- Tab Bar
-	{
-		"akinsho/bufferline.nvim",
-		dependencies = "kyazdani42/nvim-web-devicons",
-		--[[ tag = "v3.*", ]]
-		config = function()
-			local status_ok, bufferline = pcall(require, "bufferline")
-			if not status_ok then
-				return
-			end
-			vim.opt.termguicolors = true
-			bufferline.setup({
-				options = {
-					style_preset = require("bufferline").style_preset.no_italic,
-				},
-			})
-
-			-- Navigate buffers
-			vim.keymap.set("n", "<S-l>", ":bnext<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { noremap = true, silent = true })
-			vim.keymap.set("n", "<leader>x", "<cmd> bp|sp|bn|bd! <CR>", { noremap = true, silent = true })
-		end,
-	},
+	-- {
+	-- 	"akinsho/bufferline.nvim",
+	-- 	dependencies = "kyazdani42/nvim-web-devicons",
+	-- 	--[[ tag = "v3.*", ]]
+	-- 	config = function()
+	-- 		local status_ok, bufferline = pcall(require, "bufferline")
+	-- 		if not status_ok then
+	-- 			return
+	-- 		end
+	-- 		vim.opt.termguicolors = true
+	-- 		bufferline.setup({
+	-- 			options = {
+	-- 				style_preset = require("bufferline").style_preset.no_italic,
+	-- 			},
+	-- 		})
+	--
+	-- 		-- Navigate buffers
+	-- 		vim.keymap.set("n", "<S-l>", ":bnext<CR>", { noremap = true, silent = true })
+	-- 		vim.keymap.set("n", "<S-h>", ":bprevious<CR>", { noremap = true, silent = true })
+	-- 		vim.keymap.set("n", "<leader>x", "<cmd> bp|sp|bn|bd! <CR>", { noremap = true, silent = true })
+	-- 	end,
+	-- },
 	-- ----------------------------------------
 	-- File tree
 	{
