b11a20911384f527a08cf8e2440274036514fbce
mufeedcm <muhammedmufeedcm@gmail.com>
2024-12-26 21:36:18 +0530

    add:

--- linux/nvim/.config/nvim/lua/mufeedcm/plugins/markdown.lua
+++ linux/nvim/.config/nvim/lua/mufeedcm/plugins/markdown.lua
@@ -2,12 +2,18 @@ return {
 	{
 		"OXY2DEV/markview.nvim",
 		lazy = false, -- Recommended
-		-- ft = "markdown" -- If you decide to lazy-load anyway
-
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-tree/nvim-web-devicons",
 		},
+		config = function()
+			local presets = require("markview.presets")
+			require("markview").setup({
+				checkboxes = presets.checkboxes.nerd,
+				headings = presets.headings.glow,
+				horizontal_rules = presets.horizontal_rules.thin,
+			})
+		end,
 	},
 	{
 
