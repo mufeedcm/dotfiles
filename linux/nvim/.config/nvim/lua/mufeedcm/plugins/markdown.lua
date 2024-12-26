2df758883a2a3643d8041c94e5ff53b5bda5d134
mufeedcm <muhammedmufeedcm@gmail.com>
2024-12-26 20:30:00 +0530

    add:

--- linux/nvim/.config/nvim/lua/mufeedcm/plugins/markdown.lua
+++ linux/nvim/.config/nvim/lua/mufeedcm/plugins/markdown.lua
@@ -1,5 +1,15 @@
 return {
 	{
+		"OXY2DEV/markview.nvim",
+		lazy = false, -- Recommended
+		-- ft = "markdown" -- If you decide to lazy-load anyway
+
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
 
 		"MeanderingProgrammer/render-markdown.nvim",
 		enabled = false,
