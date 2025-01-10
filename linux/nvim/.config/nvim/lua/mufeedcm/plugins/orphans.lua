5c5d1dd9b7430626061c04de18f35259857c0c92
mufeedcm <muhammedmufeedcm@gmail.com>
2025-01-10 18:31:50 +0530

    auto

--- linux/nvim/.config/nvim/lua/mufeedcm/plugins/orphans.lua
+++
@@ -1,8 +0,0 @@
-return {
-	"ZWindL/orphans.nvim",
-	enabled = true,
-	branch = "fix/6-bug-stuck-on-analyzing-when-opened-inside-oilnvim",
-	config = function()
-		require("orphans").setup({})
-	end,
-}
