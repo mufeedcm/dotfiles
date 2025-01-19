ed6bfff4b39d5284c80fa1e22955513d86c6977a
mufeedcm <muhammedmufeedcm@gmail.com>
2025-01-19 18:53:55 +0530

    auto

--- linux/suckless/.config/suckless/dwm/config.h
+++ linux/suckless/.config/suckless/dwm/config.h
@@ -67,6 +67,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
+#define TERMMOD (Mod1Mask | ShiftMask)
 #define TAGKEYS(KEY, TAG)                                                      \
   {MODKEY, KEY, view, {.ui = 1 << TAG}},                                       \
       {MODKEY | ControlMask, KEY, toggleview, {.ui = 1 << TAG}},               \
@@ -79,101 +80,67 @@ static const Layout layouts[] = {
     .v = (const char *[]) { "/bin/sh", "-c", cmd, NULL }                       \
   }
 
-/* commands */
-static char dmenumon[2] =
-    "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2] = "0";
 static const char *dmenucmd[] = {
     "dmenu_run", "-m",      dmenumon, "-fn",    dmenufont, "-nb",     col_gray1,
     "-nf",       col_gray3, "-sb",    col_cyan, "-sf",     col_gray4, NULL};
-static const char *termcmd[] = {"st", NULL};
+
 /*static const char *termcmd[] = {"kitty", NULL};*/
-static const char *todocmd[] = {
-    "/bin/sh", "-c", "~/.config/suckless/dwm/scripts/open_todo.sh", NULL};
+static const char *termcmd[] = {"st", NULL};
 
-/*static const char *upvol[] = {"pactl", "set-sink-volume", "@DEFAULT_SINK@",*/
-/*                              "+10%", NULL};*/
-/*static const char *downvol[] = {"pactl", "set-sink-volume",
- * "@DEFAULT_SINK@",*/
-/*                                "-10%", NULL};*/
-/*static const char *mutevol[] = {"pactl", "set-sink-mute", "@DEFAULT_SINK@",*/
-/*                                "toggle", NULL};*/
-
+/* clang-format off */
 static const Key keys[] = {
-    /* modifier                     key        function        argument */
-    {MODKEY, XK_p, spawn, {.v = dmenucmd}},
-    {MODKEY | ShiftMask, XK_Return, spawn, {.v = termcmd}},
-    {MODKEY | ShiftMask,
-     XK_grave,
-     spawn,
-     {.v = (const char *[]){"st", "-e", "htop", NULL}}},
-    // Keybindings for Dunst
-    {MODKEY, XK_F6, spawn,
-     SHCMD("dunstctl close")}, // Close the current notification
-    {MODKEY | ShiftMask, XK_F6, spawn,
-     SHCMD("dunstctl close-all")}, // Close all notifications
-    {MODKEY, XK_F7, spawn,
-     SHCMD("dunstctl history-pop")}, // Open the notification history
-    {MODKEY, XK_F8, spawn,
-     SHCMD("dunstctl context")}, // Open the Dunst context menu
-    /*{MODKEY | ShiftMask, XK_l, spawn, SHCMD("slock")},*/
-    /*{Mod4Mask | ShiftMask, XK_s, spawn, SHCMD("slock & systemctl suspend")},*/
-    /*{Mod4Mask | ShiftMask, XK_u, spawn, SHCMD("shutdown now")},*/
-    /*{Mod4Mask | ShiftMask, XK_p, spawn,
-       SHCMD("~/.config/suckless/dwm/scripts/power_menu.sh")},*/
-    {MODKEY | ShiftMask, XK_p, spawn,
-     SHCMD("~/.config/suckless/dwm/scripts/power_menu.sh")},
+    /* modifier                     key             function        argument */
+    
+    // Applications
+    {MODKEY,                       XK_p,          spawn,          {.v = dmenucmd}},
+    {TERMMOD,                      XK_Return,     spawn,          {.v = termcmd}},
+    {TERMMOD,                      XK_grave,      spawn,          {.v = (const char *[]){"st", "-e", "htop", NULL}}},
 
-    {MODKEY | ShiftMask, XK_w, spawn,
-     SHCMD("~/.config/suckless/dwm/scripts/wifi_menu.sh")},
+    // Dunst Notifications
+    {MODKEY,                       XK_F6,         spawn,          SHCMD("dunstctl close")},
+    {TERMMOD,                      XK_F6,         spawn,          SHCMD("dunstctl close-all")},
+    {MODKEY,                       XK_F7,         spawn,          SHCMD("dunstctl history-pop")},
+    {MODKEY,                       XK_F8,         spawn,          SHCMD("dunstctl context")},
 
-    {MODKEY | ShiftMask, XK_slash, spawn,
-     SHCMD("~/.config/suckless/dwm/scripts/show_shortcuts.sh")},
+    {TERMMOD,                      XK_s,          spawn,          SHCMD("slock & systemctl suspend")},
+    {TERMMOD,                      XK_l,          spawn,          SHCMD("pkill dwm")},
+    {TERMMOD,                      XK_u,          spawn,          SHCMD("systemctl poweroff")},
+    {TERMMOD,                      XK_r,          spawn,          SHCMD("systemctl reboot")},
 
-    {MODKEY, XK_slash, spawn,
-     SHCMD("~/.config/suckless/dwm/scripts/show_shortcuts_txt.sh")},
+    // Custom Scripts
+    {MODKEY,                       XK_t,          spawn,          SHCMD("/bin/sh -c ~/.config/suckless/dwm/scripts/open_todo.sh")},
+    {TERMMOD,                      XK_w,          spawn,          SHCMD("~/.config/suckless/dwm/scripts/wifi_menu.sh")},
+    /*{TERMMOD,                      XK_p,          spawn,          SHCMD("~/.config/suckless/dwm/scripts/power_menu.sh")},*/
+    /*{TERMMOD,                      XK_slash,      spawn,          SHCMD("~/.config/suckless/dwm/scripts/show_shortcuts.sh")},*/
+    /*{MODKEY,                       XK_slash,      spawn,          SHCMD("~/.config/suckless/dwm/scripts/show_shortcuts_txt.sh")},*/
 
-    {0, XK_Print, spawn, SHCMD("flameshot gui")},
+    // Volume Controls
+    {TERMMOD,                      XK_k,          spawn,          SHCMD("~/.config/suckless/dwm/scripts/volume_bar.sh up")},
+    {TERMMOD,                      XK_j,          spawn,          SHCMD("~/.config/suckless/dwm/scripts/volume_bar.sh down")},
+    {TERMMOD,                      XK_m,          spawn,          SHCMD("~/.config/suckless/dwm/scripts/volume_bar.sh toggle")},
 
-    {MODKEY, XK_Print, spawn, SHCMD("flameshot gui -d 5000")},
-
-    {MODKEY, XK_q, spawn, {.v = todocmd}},
-    {MODKEY, XK_w, spawn,
-     SHCMD("~/.config/suckless/dwm/scripts/caffeine-mode.sh")},
-    {MODKEY, XK_space, spawn, SHCMD("rofi -show drun")},
-    /*{MODKEY | ShiftMask, XK_k, spawn, {.v = upvol}},*/
-    /*{MODKEY | ShiftMask, XK_j, spawn, {.v = downvol}},*/
-    /*{MODKEY | ShiftMask, XK_m, spawn, {.v = mutevol}},*/
-    {MODKEY | ShiftMask, XK_k, spawn,
-     SHCMD("~/.config/suckless/dwm/scripts/volume_bar.sh up")},
-    {MODKEY | ShiftMask, XK_j, spawn,
-     SHCMD("~/.config/suckless/dwm/scripts/volume_bar.sh down")},
-    {MODKEY | ShiftMask, XK_m, spawn,
-     SHCMD("~/.config/suckless/dwm/scripts/volume_bar.sh toggle")},
-    {MODKEY, XK_b, togglebar, {0}},
-    {MODKEY, XK_j, focusstack, {.i = +1}},
-    {MODKEY, XK_k, focusstack, {.i = -1}},
-    {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_d, incnmaster, {.i = -1}},
-    {MODKEY, XK_h, setmfact, {.f = -0.05}},
-    {MODKEY, XK_l, setmfact, {.f = +0.05}},
-    {MODKEY, XK_Return, zoom, {0}},
-    {MODKEY, XK_Tab, view, {0}},
-    {MODKEY | ShiftMask, XK_c, killclient, {0}},
-    {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
-    {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
-    {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
-    {MODKEY, XK_space, setlayout, {0}},
-    {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
-    {MODKEY, XK_0, view, {.ui = ~0}},
-    {MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}},
-    {MODKEY, XK_comma, focusmon, {.i = -1}},
-    {MODKEY, XK_period, focusmon, {.i = +1}},
-    {MODKEY | ShiftMask, XK_comma, tagmon, {.i = -1}},
-    {MODKEY | ShiftMask, XK_period, tagmon, {.i = +1}},
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8){MODKEY | ShiftMask, XK_q, quit, {0}},
+    // Miscellaneous
+    {MODKEY,                       XK_b,          togglebar,      {0}},
+    {MODKEY,                       XK_j,          focusstack,     {.i = +1}},
+    {MODKEY,                       XK_k,          focusstack,     {.i = -1}},
+    {MODKEY,                       XK_h,          setmfact,       {.f = -0.05}},
+    {MODKEY,                       XK_l,          setmfact,       {.f = +0.05}},
+    {MODKEY,                       XK_Return,     zoom,           {0}},
+    {MODKEY,                       XK_Tab,        view,           {0}},
+    /*{TERMMOD,                      XK_q,          quit,           {0}},*/
+    TAGKEYS(XK_1, 0)
+    TAGKEYS(XK_2, 1)
+    TAGKEYS(XK_3, 2)
+    TAGKEYS(XK_4, 3)
+    TAGKEYS(XK_5, 4)
+    TAGKEYS(XK_6, 5)
+    TAGKEYS(XK_7, 6)
+    TAGKEYS(XK_8, 7)
+    TAGKEYS(XK_9, 8)
 };
+
+/* clang-format on */
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle,
--- linux/suckless/.config/suckless/st/config.h
+++ linux/suckless/.config/suckless/st/config.h
@@ -272,47 +272,68 @@ static char *copyurlcmd[] = {"/bin/sh", "-c", "st-urlhandler -c",
 static char *copyoutput[] = {"/bin/sh", "-c", "st-copyout", "externalpipe",
                              NULL};
 
+/* clang-format off */
 static Shortcut shortcuts[] = {
     /* mask                 keysym          function        argument */
-    {XK_ANY_MOD, XK_Break, sendbreak, {.i = 0}},
-    {ControlMask, XK_Print, toggleprinter, {.i = 0}},
-    {ShiftMask, XK_Print, printscreen, {.i = 0}},
-    {XK_ANY_MOD, XK_Print, printsel, {.i = 0}},
-    {TERMMOD, XK_Prior, zoom, {.f = +1}},
-    {TERMMOD, XK_Next, zoom, {.f = -1}},
-    {TERMMOD, XK_Home, zoomreset, {.f = 0}},
-    {TERMMOD, XK_C, clipcopy, {.i = 0}},
-    {TERMMOD, XK_V, clippaste, {.i = 0}},
-    {MODKEY, XK_c, clipcopy, {.i = 0}},
-    {ShiftMask, XK_Insert, clippaste, {.i = 0}},
-    {MODKEY, XK_v, clippaste, {.i = 0}},
-    {ShiftMask, XK_Insert, selpaste, {.i = 0}},
-    {TERMMOD, XK_Num_Lock, numlock, {.i = 0}},
-    {ShiftMask, XK_Page_Up, kscrollup, {.i = -1}},
-    {ShiftMask, XK_Page_Down, kscrolldown, {.i = -1}},
-    {MODKEY, XK_Page_Up, kscrollup, {.i = -1}},
-    {MODKEY, XK_Page_Down, kscrolldown, {.i = -1}},
-    /*{MODKEY, XK_k, kscrollup, {.i = 1}},*/
-    /*{MODKEY, XK_j, kscrolldown, {.i = 1}},*/
-    {ControlMask, XK_k, kscrollup, {.i = 1}},
-    {ControlMask, XK_j, kscrolldown, {.i = 1}},
-    {MODKEY, XK_Up, kscrollup, {.i = 1}},
-    {MODKEY, XK_Down, kscrolldown, {.i = 1}},
-    {MODKEY, XK_u, kscrollup, {.i = -1}},
-    {MODKEY, XK_d, kscrolldown, {.i = -1}},
-    {MODKEY, XK_s, changealpha, {.f = -0.05}},
-    {MODKEY, XK_a, changealpha, {.f = +0.05}},
-    {TERMMOD, XK_Up, zoom, {.f = +1}},
-    {TERMMOD, XK_Down, zoom, {.f = -1}},
-    /*{TERMMOD, XK_K, zoom, {.f = +1}},*/
-    /*{TERMMOD, XK_J, zoom, {.f = -1}},*/
-    {TERMMOD, XK_U, zoom, {.f = +2}},
-    {TERMMOD, XK_D, zoom, {.f = -2}},
-    {MODKEY, XK_l, externalpipe, {.v = openurlcmd}},
-    /*{TERMMOD, XK_l, externalpipe, {.v = openurlcmd}},*/
-    {MODKEY, XK_y, externalpipe, {.v = copyurlcmd}},
-    {MODKEY, XK_o, externalpipe, {.v = copyoutput}},
+
+    /* External pipe functions */
+    { MODKEY,             XK_l,           externalpipe,   {.v = openurlcmd} },
+    { MODKEY,             XK_y,           externalpipe,   {.v = copyurlcmd} },
+    { MODKEY,             XK_o,           externalpipe,   {.v = copyoutput} },
+    
+    { TERMMOD,             XK_plus,          zoom,           {.f = +2} },
+    { TERMMOD,             XK_minus,         zoom,           {.f = -2} },
+    { TERMMOD,             XK_equal,         zoomreset,      {.f = 0} },
+
+    { MODKEY,              XK_c,          clipcopy,        {.i = 0} },
+    { MODKEY,              XK_v,          clippaste,       {.i = 0} },
+
+    { ControlMask,         XK_k,          kscrollup,      {.i = 1} },
+    { ControlMask,         XK_j,          kscrolldown,    {.i = 1} },
+
+    /* Num lock toggle */
+    { TERMMOD,           XK_Num_Lock,     numlock,        {.i = 0} },
+    
+    /* Alpha adjustment */
+    /*{ MODKEY,             XK_s,           changealpha,    {.f = -0.05} },*/
+    /*{ MODKEY,             XK_a,           changealpha,    {.f = +0.05} },*/
+
+    /* Print functions */
+    /*{ XK_ANY_MOD,          XK_Break,       sendbreak,      {.i = 0} },*/
+    /*{ ControlMask,         XK_Print,       toggleprinter,  {.i = 0} },*/
+    /*{ ShiftMask,           XK_Print,       printscreen,    {.i = 0} },*/
+    /*{ XK_ANY_MOD,          XK_Print,       printsel,       {.i = 0} },*/
+
+    /* Zoom functions */
+    /*{ TERMMOD,             XK_Prior,       zoom,           {.f = +1} },*/
+    /*{ TERMMOD,             XK_Next,        zoom,           {.f = -1} },*/
+    /*{ TERMMOD,             XK_Home,        zoomreset,      {.f = 0} },*/
+    /*{ TERMMOD,             XK_Up,          zoom,           {.f = +1} },*/
+    /*{ TERMMOD,             XK_Down,        zoom,           {.f = -1} },*/
+
+    /* Clipboard functions */
+    /*{ TERMMOD,             XK_C,          clipcopy,        {.i = 0} },*/
+    /*{ TERMMOD,             XK_V,          clippaste,       {.i = 0} },*/
+    /*{ ShiftMask,           XK_Insert,     clippaste,       {.i = 0} },*/
+    /*{ ShiftMask,           XK_Insert,     selpaste,        {.i = 0} },*/
+
+    /* Scroll functions */
+    /*{ ShiftMask,           XK_Page_Up,    kscrollup,      {.i = -1} },*/
+    /*{ ShiftMask,           XK_Page_Down,  kscrolldown,    {.i = -1} },*/
+    /*{ MODKEY,              XK_Page_Up,    kscrollup,      {.i = -1} },*/
+    /*{ MODKEY,              XK_Page_Down,  kscrolldown,    {.i = -1} },*/
+    /*{ MODKEY,              XK_Up,         kscrollup,      {.i = 1} },*/
+    /*{ MODKEY,              XK_Down,       kscrolldown,    {.i = 1} },*/
+
+    /* Miscellaneous */
+    /*{ TERMMOD,             XK_K, zoom, {.f = +1} },*/
+    /*{ TERMMOD,             XK_J, zoom, {.f = -1} },*/
+    /*{ MODKEY,             XK_k, kscrollup, {.i = 1} },*/
+    /*{ MODKEY,             XK_j, kscrolldown, {.i = 1} },*/
+    /*{ TERMMOD,             XK_l, externalpipe, {.v = openurlcmd} },*/
 };
+
+/* clang-format on */
 
 /*
  * Special keys (change & recompile st.info accordingly)
