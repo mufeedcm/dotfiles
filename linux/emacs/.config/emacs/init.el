19688a699fc46459b8f710266074e335833e4212
mufeedcm <muhammedmufeedcm@gmail.com>
2025-02-03 00:12:09 +0530

    auto

--- linux/emacs/.config/emacs/init.el
+++ linux/emacs/.config/emacs/init.el
@@ -1,188 +1,219 @@
+;;; init.el --- Modern Org-Notes Setup -*- lexical-binding: t; -*-
 
-(setq inhibit-startup-message t) ; Disable startup screen
-
-(scroll-bar-mode -1)             ; disable visible scroolbar
-(tool-bar-mode -1)               ; disbable the toolbar
-;;(tool-tip-mode -1)               ; disable tooltips
-(set-fringe-mode 0)             ; Give some breathing room
-(menu-bar-mode -1)               ; Disable the memubar
-;;(setq visible-bell t)            ; set up visible bell
-
+;;; UI SETTINGS -------------------------------------------------------------
+(setq inhibit-startup-message t)         ; Disable the startup screen
+(scroll-bar-mode -1)                      ; Disable the visible scroll bar
+(tool-bar-mode -1)                        ; Disable the toolbar
+(menu-bar-mode -1)                        ; Disable the menu bar
+(set-fringe-mode 0)                       ; Adjust fringe (if desired)
+;; (setq visible-bell t)                   ; Uncomment for a visible bell
 (setq scroll-conservatively 101)  ;; Scroll by lines, not full pages
 (setq scroll-margin 9)            ;; Keep cursor away from top/bottom
 (setq scroll-step 1)              ;; Scroll one line at a time
 (setq scroll-preserve-screen-position 1)  ;; Keep cursor position while scrolling
 
+(setq default-directory "~/notes/")
 
-;;(set-face-attribute 'default nil :font "Fira Code Retina" :height 280)
-(set-face-attribute 'default nil :height 100)  ;; Increase font size (default is ~100)
+;; Increase default font size (adjust as needed)
+(set-face-attribute 'default nil :height 100)
 
-(load-theme 'wombat)
-
-
-
-;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-
-;; Initialize package sources
+;;; PACKAGE MANAGEMENT ------------------------------------------------------
 (require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-                         ("org" . "https://orgmode.org/elpa/")
-                         ("elpa" . "https://elpa.gnu.org/packages/")))
-
+(setq package-archives
+      '(("melpa" . "https://melpa.org/packages/")
+        ("org"   . "https://orgmode.org/elpa/")
+        ("gnu"   . "https://elpa.gnu.org/packages/")))
 (package-initialize)
 (unless package-archive-contents
- (package-refresh-contents))
+  (package-refresh-contents))
 
-;; Initialize use-package on non-Linux platforms
+;; Bootstrap use-package if needed.
 (unless (package-installed-p 'use-package)
-
   (package-install 'use-package))
-
 (require 'use-package)
 (setq use-package-always-ensure t)
 
-
-(column-number-mode)
-(setq display-line-numbers-type 'relative)
-(global-display-line-numbers-mode t)
-
-;; Disable line numbers for some modes
-(dolist (mode '(org-mode-hook
-                term-mode-hook
-    shell-mode-hook
-                eshell-mode-hook))
-  (add-hook mode (lambda () (display-line-numbers-mode 0))))
-
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-
-;; NOTE: The first time you load your configuration on a new machine, you'll
-;; need to run the following command interactively so that mode line icons
-;; display correctly:
-;; M-x all-the-icons-install-fonts
-;;(use-package all-the-icons)
-;;M-x nerd-icons-install-fonts
-(use-package nerd-icons)
-
-(use-package doom-modeline
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
-
+;;; THEME -------------------------------------------------------------------
+;; Load Doom Tokyo-Night theme (your preferred look)
 (use-package doom-themes
-  :init (load-theme 'doom-tokyo-night t))
+  :config
+  (load-theme 'doom-tokyo-night t))
 
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
+;;; MODERN COMPLETION & SEARCH ---------------------------------------------
+;; We replace Ivy/Swiper with a leaner stack.
+(use-package vertico
+  :init
+  (vertico-mode)
+  (setq enable-recursive-minibuffers t))
 
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-category-defaults nil)
+  (completion-category-overrides '((file (styles partial-completion)))))
+
+(use-package marginalia
+  :init (marginalia-mode))
+
+(use-package consult
+  :bind (("C-s"     . consult-line)
+         ("C-x b"   . consult-buffer)
+         ("C-x C-r" . consult-recent-file)
+         ("C-c r"   . consult-ripgrep))  ;; Ripgrep search across files.
+  :init
+  (setq consult-project-root-function #'vc-root-dir))
+
+(use-package embark
+  :bind (("C-." . embark-act))
+  :init
+  (setq prefix-help-command #'embark-prefix-help-command))
+
+(use-package embark-consult
+  :after (embark consult)
+  :demand t
+  :hook (embark-collect-mode . consult-preview-at-point-mode))
+
+;;which key
 (use-package which-key
   :init (which-key-mode)
   :diminish which-key-mode
   :config
   (setq which-key-idle-delay 1))
 
-(use-package ivy-rich
-  :init
-  (ivy-rich-mode 1))
 
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history)))
-
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-
-
-(use-package general
-  :config
-  (general-create-definer rune/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-
-  (rune/leader-keys
-    "t"  '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose theme")))
-
+;;; VIM-STYLE EDITING ---------------------------------------------
 (use-package evil
   :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
+  (setq evil-want-integration t
+        evil-want-keybinding nil
+        evil-want-C-u-scroll t)
   :config
-  (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
+  (evil-mode 1))
 
 (use-package evil-collection
   :after evil
   :config
+  (define-key evil-normal-state-map (kbd "gc") 'comment-line)
   (evil-collection-init))
 
-(use-package hydra)
 
-(defhydra hydra-text-scale (:timeout 4)
-  "scale text"
-  ("j" text-scale-increase "in")
-  ("k" text-scale-decrease "out")
-  ("f" nil "finished" :exit t))
+;;undo
+(use-package undo-tree
+  :init
+  (global-undo-tree-mode)
+  :config
+  (setq undo-tree-auto-save-history nil)) ;; Don't save undo history to files
 
-(rune/leader-keys
-  "ts" '(hydra-text-scale/body :which-key "scale text"))
-
-
+;; Evil integration
+(evil-set-undo-system 'undo-tree)
 
 
+;;; Keybindings
 
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+(use-package general
+  :config
+  (general-create-definer my/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+  (my/leader-keys
+    "oo"  '(find-file :which-key "Find file")
+    "b"   '(:ignore t :which-key "Buffers")
+    "bb"  '(switch-to-buffer :which-key "Switch buffer")
+    "w"   '(:ignore t :which-key "Windows")
+    "wv"  '(split-window-right :which-key "Vertical split")
+    "ws"  '(split-window-below :which-key "Horizontal split")
+    "wc"  '(delete-window :which-key "Close window")
+    "ww" '(other-window :which-key "Switch window")
+    "wh" '(windmove-left :which-key "Move left")
+    "wl" '(windmove-right :which-key "Move right")
+    "wj" '(windmove-down :which-key "Move down")
+    "wk" '(windmove-up :which-key "Move up")
+    ;; Custom keys for Org management:
+    "oa"  '(org-agenda :which-key "Org Agenda")
+    "oc"  '(org-capture :which-key "Org Capture")
+    "on"  '(consult-notes :which-key "Open Notes")))
 
+;;; DOOM MODELINE ----------------------------------------------------------
+(use-package doom-modeline
+  :init (doom-modeline-mode 1)
+  :custom (doom-modeline-height 15))
 
+;;; ORG MODE CONFIGURATION --------------------------------------------------
+(defun my/org-mode-setup ()
+  "Custom setup for Org mode."
+  (org-indent-mode)
+  (visual-line-mode 1)
+  (variable-pitch-mode 1))
 
+(use-package org
+  :hook (org-mode . my/org-mode-setup)
+  :config
+  ;; Visual tweaks.
+  (setq org-ellipsis " ▾"
+        org-hide-leading-stars t)
+  ;; Agenda files include your personal tasks and diploma/semester notes.
+  (setq org-agenda-files '("~/notes/org/tasks.org"
+                           "~/notes/org/diploma/sem4/sem4.org"))
+  ;; Archive completed tasks to a dedicated archive file.
+  (setq org-archive-location "~/notes/org/archive.org::")
+  ;; Org Capture templates.
+  (setq org-capture-templates
+        '(("t" "General Task" entry
+           (file+headline "~/notes/org/tasks.org" "Inbox")
+           "* TODO %?\n  %U\n  %i\n  %a")
+          ("d" "Diploma Task" entry
+           (file+headline "~/notes/org/diploma/sem4/sem4.org" "Inbox")
+           "* TODO %?\n  %U\n  %i\n  %a")))
+  (setq org-log-done 'time))  ; Record timestamp when a task is marked done.
+
+(use-package org-bullets
+  :after org
+  :hook (org-mode . org-bullets-mode)
+  :custom
+  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
+
+(use-package visual-fill-column
+  :hook (org-mode . visual-fill-column-mode)
+  :custom
+  (visual-fill-column-width 100)
+  (visual-fill-column-center-text t))
+
+;;; DASHBOARD: WELCOME SCREEN FOR NOTES MANAGEMENT --------------------------
+;; The dashboard package gives you a start screen with quick shortcuts.
+(use-package dashboard
+  :init
+  (setq dashboard-startup-banner "") ;; No banner at all
+  (setq dashboard-center-content t)  ;; Doom-like centered content
+  (setq dashboard-set-heading-icons t)
+  (setq dashboard-set-file-icons t)
+  (setq dashboard-items '((agenda . 7)   ;; Show next 7 agenda items
+                          (recents  . 5) ;; Recent files
+                          (bookmarks . 5))) ;; Bookmarks for quick access
+  (setq dashboard-footer-messages '("")) ;; No GNU propaganda
+  :config
+  (dashboard-setup-startup-hook))
+
+;;; CUSTOM FUNCTIONS FOR NOTES MANAGEMENT -------------------------------
+(defun consult-notes ()
+  "Open your main Org tasks file."
+  (interactive)
+  (find-file "~/notes/org/tasks.org"))
+
+;;; ADDITIONAL CUSTOMIZATIONS -----------------------------------------------
+;; (Add further keybindings or configurations here as desired)
+
+(provide 'init)
+;;; init.el ends here
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("4594d6b9753691142f02e67b8eb0fda7d12f6cc9f1299a49b819312d6addad1d" "48042425e84cd92184837e01d0b4fe9f912d875c43021c3bcb7eeb51f1be5710" default))
  '(package-selected-packages
-   '(hydra evil-collection evil general helpful counsel ivy-rich which-key rainbow-delimiters doom-themes doom-modeline ivy)))
+   '(undo-tree which-key visual-fill-column vertico org-bullets orderless marginalia general evil-collection embark-consult doom-themes doom-modeline dashboard)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
