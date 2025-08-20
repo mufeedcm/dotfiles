616eb7b9640004c10871d5cc889570a2e5e7ccf8
mufeedcm <muhammedmufeedcm@gmail.com>
2025-08-20 07:24:46 +0530

    add: more ----> rofi

--- linux/scripts/.config/scripts/confirm.sh
+++ linux/scripts/.config/scripts/confirm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-choice=$(echo -e "Yes\nNo" | dmenu -i -p "$1?")
+# choice=$(echo -e "Yes\nNo" | dmenu -i -p "$1?")
+choice=$(echo -e "Yes\nNo" | rofi -dmenu -i -p "$1?")
 
 if [ "$choice" = "Yes" ]; then
     eval "$2"
