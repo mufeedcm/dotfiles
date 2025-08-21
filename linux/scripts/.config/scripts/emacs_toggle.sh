83460378b1528b3b6ba3c58e2984b8818748227f
mufeedcm <muhammedmufeedcm@gmail.com>
2025-08-21 23:42:01 +0530

    fix: more-stuff

--- linux/scripts/.config/scripts/emacs_toggle.sh
+++ linux/scripts/.config/scripts/emacs_toggle.sh
@@ -1,11 +1,39 @@
 #!/bin/bash
+
 TITLE="scratchpad-emacs"
-WIN_ID=$(i3-msg -t get_tree | grep -oP '"id":\K\d+(?=,.*"title":"'"$TITLE"'")')
+LOCKFILE="/tmp/scratchpad-emacs.lock"
+
+# Simple lock to prevent multiple launches
+exec 200>"$LOCKFILE"
+flock -n 200 || exit 0  # if already locked, exit immediately
+
+# Get the window ID of Emacs with the given title
+WIN_ID=$(i3-msg -t get_tree | jq -r '.. | objects | select(.window_properties?.title=="'"$TITLE"'") | .id')
 
 if [ -z "$WIN_ID" ]; then
+    # Window doesn't exist: create it
     emacs --name "$TITLE" -g 120x45 &
-    sleep 0.5  
-    i3-msg "[title=\"$TITLE\"] floating enable, resize set 1200 800, move position center, move scratchpad"
+    
+    # Wait until the window appears in i3
+    while : ; do
+        WIN_ID=$(i3-msg -t get_tree | jq -r '.. | objects | select(.window_properties?.title=="'"$TITLE"'") | .id')
+        [ -n "$WIN_ID" ] && break
+        sleep 0.1
+    done
+    
+    # Make it floating, resize, center, move to scratchpad, then show
+    i3-msg "[title=\"$TITLE\"] floating enable, resize set 1100 900, move position center, move scratchpad, scratchpad show"
 else
-    i3-msg "[title=\"$TITLE\"] scratchpad show"
+    # Window exists, check if it's visible
+    VISIBLE=$(i3-msg -t get_tree | jq '.. | objects | select(.id=='"$WIN_ID"') | .visible')
+    
+    if [ "$VISIBLE" = "true" ]; then
+        # Window is visible: move it to scratchpad
+        i3-msg "[title=\"$TITLE\"] move scratchpad"
+    else
+        # Window hidden: show it, then center
+        i3-msg "[title=\"$TITLE\"] scratchpad show, move position center"
+    fi
 fi
+
+# Release lock automatically at script exit
