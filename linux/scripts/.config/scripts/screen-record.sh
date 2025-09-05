4c9faa10c42a3086e87595220871a59304e37e41
mufeedcm <muhammedmufeedcm@gmail.com>
2025-09-05 23:03:08 +0530

    add: screen-record script

---
+++ linux/scripts/.config/scripts/screen-record.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+record() {
+  ffmpeg \
+    -video_size 1920x1080 \
+    -framerate 30 \
+    -f x11grab -i :0.0 \
+    -f pulse -i default \
+    -c:v h264 -qp 0 \
+    -c:a aac \
+    "$HOME/Videos/video_$(date '+%a__%b%d__%H_%M_%S').mkv" &
+
+  echo $! > /tmp/recpid
+
+  notify-send -t 500 -h string:bgcolor:#a3be8c "Screen recording started"
+}
+
+end() {
+  kill -15 "$(cat /tmp/recpid)" && rm -f /tmp/recpid
+
+  notify-send -t 500 -h string:bgcolor:#bf616a "Screen recording stopped"
+}
+
+([[ -f /tmp/recpid ]] && end && exit 0) || record
